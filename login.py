6a2c2ebae28ec959d93402594de0c049feca2f7f
Lei.Wang99 <821609514@qq.com>
2023-08-03 10:33:19 +0800

    fix

--- login.py
+++ login.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
         config.set(mobile, 'lat', location.split(',')[1])
         config.set(mobile, 'lng', location.split(',')[0])
         config.write(open(path, 'w+', encoding="utf-8"))  # 保存数据
-        condition = input(f"是否继续添加账号[Y/N]:").lstrip().rstrip()
+        condition = input(f"是否继续添加账号[y/n]:").lstrip().rstrip()
         condition = condition.lower()
         if condition == 'n':
             break
