3db2966a0d4364c0f49e50ea1cc5f2a6a7077384
Lei.Wang99 <821609514@qq.com>
2023-08-16 17:11:15 +0800

    perf: 优化获取配置方法
    之前每次都要去创建一次路径，然后忽略报错。
    
    BREAKING CHANGE: 每次先判断是否存在，如果有直接返回路径

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import configparser
-
 import os
 import config as cf
 import process
@@ -12,24 +11,23 @@ def get_credentials_path():
         return cf.CREDENTIALS_PATH
     else:
         home_path = os.getcwd()
-        path = os.path.join(home_path, 'myConfig', 'credentials')
-        # 尝试创建目录
-        try:
+        config_path = os.path.join(home_path, 'myConfig', 'credentials')
+        if os.path.exists(config_path):
+            return config_path
+        else:
             os.mkdir(os.path.join(home_path, 'myConfig'))
-        except OSError:
-            pass
-        return path
+            return config_path
 
 
 path = get_credentials_path()
+# 这里config需要用encoding，以防跨平台乱码
 config.read(path, encoding="utf-8")
 sections = config.sections()
 
 
 def get_location():
     while 1:
-
-        location = input(f"请输入你的位置，例如[小区名称]，为你自动预约附近的门店:").lstrip().rstrip()
+        location = input(f"请输入精确小区位置，例如[小区名称]，为你自动预约附近的门店:").strip()
         selects = process.select_geo(location)
 
         a = 0
@@ -38,7 +36,7 @@ def get_location():
             province = item['province']
             print(f'{a} : [地区:{province},位置:{formatted_address}]')
             a += 1
-        user_select = input(f"请选择位置序号,重新输入请输入[-]:").lstrip().rstrip()
+        user_select = input(f"请选择位置序号,重新输入请输入[-]:").strip()
         if user_select == '-':
             continue
         select = selects[int(user_select)]
@@ -57,9 +55,9 @@ if __name__ == '__main__':
         city = location_select['city']
         location: str = location_select['location']
 
-        mobile = input("输入手机号[13812341234]:").lstrip().rstrip()
+        mobile = input("输入手机号[13812341234]:").strip()
         process.get_vcode(mobile)
-        code = input(f"输入 [{mobile}] 验证码[1234]:").lstrip().rstrip()
+        code = input(f"输入 [{mobile}] 验证码[1234]:").strip()
         token, userId = process.login(mobile, code)
         if mobile not in sections:
             config.add_section(mobile)  # 首先添加一个新的section
@@ -71,7 +69,7 @@ if __name__ == '__main__':
         config.set(mobile, 'lat', location.split(',')[1])
         config.set(mobile, 'lng', location.split(',')[0])
         config.write(open(path, 'w+', encoding="utf-8"))  # 保存数据
-        condition = input(f"是否继续添加账号[y/n]:").lstrip().rstrip()
-        condition = condition.lower()
-        if condition == 'n':
+        condition = input(f"是否继续添加账号[y/n]:").strip()
+
+        if condition.lower() == 'n':
             break
