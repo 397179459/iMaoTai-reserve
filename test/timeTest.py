9216fd8db901d068e666f4a9907358a698c17379
397179459 <821609514@qq.com>
2023-08-19 22:16:30 +0800

    test: 增加日期测试用例

--- test/timeTest.py
+++ test/timeTest.py
@@ -7,3 +7,7 @@ import datetime
 print(time.time())
 print(time.mktime(datetime.date.today().timetuple()))
 print(int(time.mktime(datetime.date.today().timetuple())) * 1000)
+
+s = datetime.date.today().strftime("%Y%m%d")
+print(type(s))
+
