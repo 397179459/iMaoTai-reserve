53af0e767a4903848636572ecf91ca92f6c4ae8b
Lei.Wang99 <821609514@qq.com>
2023-08-23 14:29:50 +0800

    docs: 增加函数注释

--- privateCrypt.py
+++ privateCrypt.py
@@ -7,7 +7,11 @@ import logging
 
 
 def get_aes_key():
-    private_key = config.PRIVATE_AES_KEY  # 你的密钥
+    """
+    获取config中你的AES私钥
+    :return:
+    """
+    private_key = config.PRIVATE_AES_KEY  # 你的私钥
     if private_key is None:
         logging.error("!!!!请配置config.py中PRIVATE_AES_KEY(AES的私钥)")
         raise ValueError
@@ -16,12 +20,24 @@ def get_aes_key():
 
 
 def encrypt_aes_ebc(plain_str, key):
+    """
+    无偏移的AES加密
+    :param plain_str: 需要加密的明文
+    :param key: AES私钥
+    :return: base64后的密文
+    """
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = cipher.encrypt(pad(plain_str.encode(), AES.block_size))
     return base64.b64encode(ciphertext).decode()
 
 
 def decrypt_aes_ebc(ciphertext, key):
+    """
+    无偏移的AES解密
+    :param ciphertext: 需要解密的密文
+    :param key: AES私钥
+    :return: 解密后的明文
+    """
     ciphertext = base64.b64decode(ciphertext)
     cipher = AES.new(key, AES.MODE_ECB)
     plain_str = unpad(cipher.decrypt(ciphertext), AES.block_size)
