34e9da1da66b7151958ff55426e9cd9afe349354
Yu haishuai <1535188660@qq.com>
2024-01-11 20:49:42 +0800

    修改个人配置文件

--- config.py
+++ config.py
@@ -19,14 +19,14 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
 PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
-
+PUSH_TOKEN = 'edb956c697a04268b851be438de4fb67'
 
 '''
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
-AMAP_KEY = os.environ.get("GAODE_KEY")
-
+#AMAP_KEY = os.environ.get("bef9cbb9d786f89cf73fff347373ee7b")
+AMAP_KEY = '2068c9b7f292c679e7bf3882bcac3403'
 
 '''
 *********** 个人账户认证配置 ***********
@@ -34,7 +34,7 @@ AMAP_KEY = os.environ.get("GAODE_KEY")
 不配置,使用默认路径,在项目目录中;如果需要配置,你自己应该也会配置路径
 例如： CREDENTIALS_PATH = './myConfig/credentials'
 '''
-CREDENTIALS_PATH = None
+CREDENTIALS_PATH = './myConfig/credentials'
 
 
 '''
@@ -45,7 +45,8 @@ key用了SHA-256转化,所以这里可以配置任意字符串,不用遵守AES�
 如果不会配置环境变量(建议学习)、不care安全性、非开源运行,你可以在这里明文指定,eg:PRIVATE_AES_KEY = '666666'
 ps:本来是写了判断是否配置密钥，可以自由选择明文保存的方式。但是还是为了安全性，限制了必须使用AES加密。哪怕是明文密钥。
 '''
-PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
+#PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
+PRIVATE_AES_KEY = '1010697281'
 
 
 '''
