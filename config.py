741709cf35f9ffa95aa9dbb8f60a2fb1f744784e
sidianwindshui <82257229+sidianwindshui@users.noreply.github.com>
2024-01-29 09:19:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,15 +18,15 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
-
+# PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
+PUSH_TOKEN = '0f51ff8a4873494482863ecd003f1f69'
 
 '''
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
 AMAP_KEY = os.environ.get("GAODE_KEY")
-
+AMAP_KEY = 'a5b3ad8d8a766c6716cdc473b949ac8c'
 
 '''
 *********** 个人账户认证配置 ***********
@@ -45,8 +45,8 @@ key用了SHA-256转化,所以这里可以配置任意字符串,不用遵守AES�
 如果不会配置环境变量(建议学习)、不care安全性、非开源运行,你可以在这里明文指定,eg:PRIVATE_AES_KEY = '666666'
 ps:本来是写了判断是否配置密钥，可以自由选择明文保存的方式。但是还是为了安全性，限制了必须使用AES加密。哪怕是明文密钥。
 '''
-PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
-
+# PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
+PRIVATE_AES_KEY = 'yx671149'
 
 '''
 *********** 预约规则配置 ************
