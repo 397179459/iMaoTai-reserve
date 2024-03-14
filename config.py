022b46806d9716541129a2b4a71616aac840b80c
Auchao95 <61224906+Auchao95@users.noreply.github.com>
2024-03-14 15:14:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
-
+#PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
+PUSH_TOKEN = 'c5d8ab99d78b4aca9d9df98b41dbc1de'
 
 '''
 *********** 地图配置 ***********
