d8b989333001c0b39780529f7e479a63fe83024c
Lsong <liuchao_huoguo@163.com>
2024-08-26 09:47:41 +0800

    Revert "refactor: replace pusher"
    This reverts commit 029c04ef64abe53c1eb18d6e41567450baef2ab7.

--- config.py
+++ config.py
@@ -18,14 +18,14 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 如没有配置则不推送消息
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
-PUSH_TOKEN = "SCT60342TL0ntgMmeKY5DoqoSkAbF42yz"
+PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
 
 
 '''
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
-AMAP_KEY = "dd302713938de1f23313ee51e7127940"
+AMAP_KEY = os.environ.get("GAODE_KEY")
 
 
 '''
@@ -45,7 +45,7 @@ key用了SHA-256转化,所以这里可以配置任意字符串,不用遵守AES�
 如果不会配置环境变量(建议学习)、不care安全性、非开源运行,你可以在这里明文指定,eg:PRIVATE_AES_KEY = '666666'
 ps:本来是写了判断是否配置密钥，可以自由选择明文保存的方式。但是还是为了安全性，限制了必须使用AES加密。哪怕是明文密钥。
 '''
-PRIVATE_AES_KEY = "dd302713938de1f23313ee51e7127940"
+PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
 
 
 '''
@@ -53,7 +53,7 @@ PRIVATE_AES_KEY = "dd302713938de1f23313ee51e7127940"
 因为目前支持代提的还是少,所以建议默认预约最近的门店
 '''
 _RULES = {
-    'MIN_DISTANCE': 1,   # 预约你的位置最近的门店
-    'MAX_SALES': 0,      # 预约本市出货量最大的门店
+    'MIN_DISTANCE': 0,   # 预约你的位置最近的门店
+    'MAX_SALES': 1,      # 预约本市出货量最大的门店
 }
 RESERVE_RULE = 0         # 在这里配置你的规则，只能选择其中一个
